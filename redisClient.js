0e66bbad4a497c9217ab920f13060782e3269cb4
Pedro Perillo <pedrofperillo@gmail.com>
2024-06-27 11:42:10 -0600

    added changes

--- redisClient.js
+++ redisClient.js
@@ -4,8 +4,7 @@ const REDIS_HOST = "127.0.0.1";
 const REDIS_PORT = 6379;
 // Create Redis Client & Handle Connection Errors
 const redisClient = createClient({
-    host: REDIS_HOST,
-    port: REDIS_PORT
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`
 })
 
 redisClient.connect().then(r => {
